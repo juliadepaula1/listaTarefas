2d6c3939f8518fa45970528e3076dbe05320fb6a
juliadepaula1 <jujupaulade0706@gmail.com>
2024-11-04 21:30:39 -0300

    Adicionando botoes e suas funcionalidades, e ajustando estrutura e erros.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,50 @@
 let count = 0;
+let spanRemovido = null;
+
 
 function adicionar() {
+    // Recuperando o valor do elemenbto tarefa
     let tarefa = document.getElementById("tarefa").value;
 
+    //Verificar se existe uma tarefa digitada
     if(tarefa) {
+
+        // COntador do id tarefa
         count++;
+
+        //recuperando lista de ul lista de tarefas
         let lista = document.getElementsByClassName("tarefas")[0];
 
+        // criando os elementos li e button
         let li = document.createElement("li");
         let btn = document.createElement("button");
 
+        //Atribuindo valores para o elemento LI
+        li.id = count;
+        li.className = "tarefa-item";
+        li.textContent = tarefa;
+
+        //atribuindo valores para o elemento button 
+        btn.className ="btn";
+        btn.textContent = "remover";
+        btn.onclick = () => {
+            lista.removeChild(li);
+        }
+
+        //adicionando o buttojn criado no li 
+        li.appendChild(btn);
+
+        //Adicionando o li criado na lista
+        lista.appendChild(li);
+
+        let mensagemNaoExiste = document.getElementById("nao-existe")
+        if(mensagemNaoExiste){
+            spanRemovido = mensagemNaoExiste;
+            mensagemNaoExiste.remove();
+        }
     } else {
         alert("Insira uma tarefa!")
     }
+
+    document.getElementById("tarefa").value = "";
 }
